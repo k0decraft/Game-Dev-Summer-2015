5d985c16006ec96dfc6aee1567bcb2e215e956a4
Bdtzau <user@users-MBP.attlocal.net>
2015-08-25 11:24:29 -0400

    Props and Organization
    Added prefabs for props and significantly revamped file organization

---
+++ Assets/Scripts/character_controller.js
@@ -0,0 +1,60 @@
+﻿#pragma strict
+
+var moveSpeed:float = 10;
+var maxSpeed:float = 50;
+var fireRate:float = 0.5;
+var bulletSpawn:GameObject;
+var bullet:GameObject;
+var aimer:GameObject;
+var sprite:GameObject;
+
+private var rb:Rigidbody;
+private var animator:Animator;
+private var nextFire:float = 0.0;
+
+function Start () {
+	rb = GetComponent(Rigidbody);
+
+	animator = sprite.GetComponent(Animator);
+	
+	Debug.Log(Input.GetJoystickNames());
+}
+
+function FixedUpdate () {
+
+	// movement
+	var movementVector:Vector3;
+
+	if (Input.GetAxis("Horizontal")) {
+		movementVector.x = Input.GetAxis("Horizontal");
+	}
+	if (Input.GetAxis("Vertical")) {
+		movementVector.z = Input.GetAxis("Vertical");
+	}
+	if (movementVector.magnitude > 0.001) {
+		Vector3.Normalize(movementVector);
+		movementVector *= moveSpeed;
+		rb.velocity = movementVector;
+		// rb.AddForce(movementVector);
+	}
+
+
+
+	// face mouse
+	var aim:Vector3 = Input.mousePosition;
+	
+	aim.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
+	aim = Camera.main.ScreenToWorldPoint(aim);
+	aimer.transform.LookAt(aim);
+
+	Debug.Log(aimer.transform.eulerAngles.y);
+
+	animator.SetFloat("aimDirection", aimer.transform.eulerAngles.y);
+
+
+	// shooting
+	if (Input.GetButton("Fire1") && Time.time > nextFire) {
+		nextFire = Time.time + fireRate;
+		var bulletClone = Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+	}
+}
